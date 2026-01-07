b9d232994c49b26a0b62f476372649a7782ebf83
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-07 14:57:46 +0530

    Add files via upload

---
+++ 02_practicals/08.DSA PYTHON/7.Intro Queue.py
@@ -0,0 +1,17 @@
+# What is Queue?
+# queue is defined as a linear data structure that is open at both ends and the 
+# operations are performed in First In First Out (FIFO) order.
+
+
+# We define a queue to be a list in which all additions to the list are made at one end, 
+# and all deletions from the list are made at the other end.  The element which is first 
+# pushed into the order, the operation is first performed on that.
+
+# Characteristics of Queue:
+# 1.Queue can handle multiple data.
+# 2.We can access both ends.
+# 3.They are fast and flexible. 
+
+# Queue Basic operation := 
+# 1. enqueue() ::: Adding Element
+# 2. dequeue() ::: Removing Element
